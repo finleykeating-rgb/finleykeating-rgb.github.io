cb90a32728913f2caf59c04adfea2c2bb644ec09
fke20 <fke20@uclive.ac.nz>
2026-02-11 12:47:54 +1300

    Added more text and images to award/extra curricular page

--- finley-keating-personal/src/app/awards/page.tsx
+++ finley-keating-personal/src/app/awards/page.tsx
@@ -1,39 +1,57 @@
+"use client"
 import NavBar from "../ui/nav-bar";
 import Image from "next/image";
+import useEmblaCarousel from 'embla-carousel-react'
+import { useCallback } from "react";
+
 export default function awards() {
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false })
+
+  const scrollPrev = useCallback(() => {
+    emblaApi?.scrollPrev()
+  }, [emblaApi])
+
+  const scrollNext = useCallback(() => {
+    emblaApi?.scrollNext()
+  }, [emblaApi])
+
   return (
     <div id="main-layout">
       <NavBar/>
       <main>
         <div className="container">
           <h1>School Activities</h1>
-          <div>
-            <h1>When I was a young boy, my mother taught me how to play chess. 
-                In my primary school I joined the chess club and competed as the second board on the school team. 
-                The school teams was very successful, reaching nationals two years in a row. I even did one tournament at a chess club which was mostly against adults. 
-                I enjoy the competition of chess. It is a simple battle of wits with no chance involved. 
-                I still occasionally play to this day on <a style={{color: "blue"}} href={"https://lichess.org/@/AcidRainSSJ/perf/rapid"}>Lichess</a> but mostly puzzles.</h1>
-            <div>
+
+          
+          
+          
+          <div className="image-grid">
+            <div style={{gridColumn: 0, gridRow: "span 3" }}>
+              <h1>When I was a young boy, my mother taught me how to play chess. 
+              In my primary school I joined the chess club. My school (Waimairi school), competed in a number of interschool tournaments. 
+              School tournament are done as team tournaments. They are done in teams of 4. You are ordered by order of strength 1-4. Each teams 1 plays the 1, 2 plays 2 and so on. I was the 2.
+              Our team was quite successful. We qualified for the nationals two years in a row (2012, 2013) and won the south island championship in 2013.
+              I even did one tournament at a local chess club. This tournament consisted of mostly adults. I managed to get 15th place out of 20.  
+              I enjoyed the creativity and competition of chess.   
+              I still occasionally play to this day on <a style={{color: "blue"}} href={"https://lichess.org/@/AcidRainSSJ/perf/rapid"}>Lichess</a> but mostly puzzles.
+              </h1>
+            </div>
+            <div style={{gridColumn: 2, }}>
                 <Image src={"/resources/chess_national_photo.png"} width={679} height={508} alt={"Chess National 2012"}/>
-                <p>Playing chess in the park at the square in Parmeston North (Left to Right, Jonathan, James, Rocco, Finley), 2012</p>
-            </div>
-            <div>
+                <p className="image-subtitle">Playing chess in the park at the square in Parmeston North, 2012</p>
                 <Image src={"/resources/chess_standing.png"} width={679} height={407} alt={"Chess South Island 2013"}/>
-                <p>Standing up to make a move at the South Islands, 2013</p>
+                <p className="image-subtitle">Standing up to make a move at the South Islands, 2013</p>
             </div>
-            <div>
-                <Image src={"/resources/only_chess_tournament.png"} width={679} height={407} alt={"Chess South Island 2013"}/>
-                <p>The <a style={{color: "blue"}} href={"https://newzealandchess.nz/2012/results2012.html"}>results</a> from my first and only official tournament, all adults except 4, 2012</p>
-            </div>
-            <div>
+            <div style={{gridColumn: 3, gridRow: "span 3"}}>
+                <Image src={"/resources/only_chess_tournament.png"} width={679} height={407} alt={"Chess Tournament"}/>
+                <p className="image-subtitle">The <a style={{color: "blue"}} href={"https://newzealandchess.nz/2012/results2012.html"}>results</a> from my first and only official tournament 2012</p>
                 <Image src={"/resources/chess_nationals_2012.png"} width={679} height={407} alt={"Chess Nationals 2012"}/>
-                <p>The <a style={{color: "blue"}} href={"https://newzealandchess.nz/2012/results2012.html"}>results</a> from my first chess nationals. Playing for Waimairi, 2012</p>
-            </div>
-            <div>
                 <Image src={"/resources/chess_nationals_2013.png"} width={679} height={407} alt={"Chess Nationals 2013"}/>
-                <p>The <a style={{color: "blue"}} href={"https://newzealandchess.nz/2013/results2013.html"}>results</a> from my second chess nationals. Playing for Waimairi, 2013</p>
+                <p className="image-subtitle">The <a style={{color: "blue"}} href={"https://newzealandchess.nz/2013/results2013.html"}>results</a> from my chess nationals. Playing for Waimairi, 2013</p>
             </div>
+             
           </div>
+          
           <div>
             <h1>AIMS Scholarship </h1>
             <h1>https://scholarshipscanterbury.communityforce.com/Funds/FundDetails.aspx?496F49596D593768464E3537506B6F754D5442315A354B58664355313053303153314F5852446850507453696E756249564E346F2B583764686173553574787143706D657756356C3739633D</h1>
