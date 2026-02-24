41394e734b3fbfb21a0090f5703ac33464e0c2e7
fke20 <fke20@uclive.ac.nz>
2026-02-24 14:30:45 +1300

    Added main page styling

--- finley-keating-personal/src/app/page.tsx
+++ finley-keating-personal/src/app/page.tsx
@@ -1,20 +1,49 @@
 
 import NavBar from "./ui/nav-bar";
+import main_page from "@public/music_descriptions/main_page.json";
+
+interface pdf {
+  link: string
+  text: string
+}
+
+interface main {
+  title: string
+  subtitle: string
+  website_overview: string
+  cv: pdf
+  disclaimer: string
+}
 
 export default function Home() {
+  const main_content : main = main_page
+
   return (
     <div id="main-layout">
       <NavBar/>
       <main>
-        <div className="">
+        <div className="main_page">
           <div>
             <div>
-              <h1>
-                Welcome All
+              <h1 className="main_title">
+                {main_content.title}
               </h1>
-              <h2>
-                Hi, I am Finley, a software developer based in Christchurch, New Zealand. I enjoy problem solving, listening to music and learning about the world.
+              <h3 className="main_subtitle">
+                {main_content.subtitle}
+              </h3>
+              <h2 className="main_overview">
+                {main_content.website_overview}
               </h2>
+              <a className="main_cv" 
+                href={main_content.cv.link}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                {main_content.cv.text}
+              </a>
+              <h3 className="main_disclaimer">
+                {main_content.disclaimer}
+              </h3>
             </div>
           </div>
         </div>
